515e7079c24282645d068527fd57d3735d0e4d3e
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-11-28 13:16:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "azurerm" {
 
 variable "resource_group_name" {}
 
-resource "azurerm_resource_group" "sqlmi" {
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = "East US"
 }
 
-resource "azurerm_network_security_group" "sqlmi" {
+resource "azurerm_network_security_group" "example" {
   name                = "mi-security-group"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
